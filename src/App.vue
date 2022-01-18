93776bb8e110eb4c1a6dc367a11399c808235e28
TibebeJs <Tibebes.js@gmail.com>
2022-01-18 16:06:40 +0300

    chore: setup tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
 <script>

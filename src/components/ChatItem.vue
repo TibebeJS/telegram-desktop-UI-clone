b9a6e1d69a71dbc8b2073dd7d5c60dd949509b40
TibebeJs <Tibebes.js@gmail.com>
2022-01-18 17:02:00 +0300

    feat: create chats list and search box

---
+++ src/components/ChatItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="px-4 flex gap-2">
+        <div>
+            <img
+                v-if="chat.profileImage"
+                :src="chat.profileImage"
+                :alt="`${chat.title} profile image`"
+                class="w-11 h-11 rounded-full"
+            />
+            <div v-else></div>
+        </div>
+        <div class="flex flex-col">
+            <div>
+                {{ chat.title }}
+            </div>
+            <div class="text-gray-400">
+                {{ chat.lastMessage }}
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ["chat"],
+    setup() {
+
+    },
+}
+</script>
---
+++ src/components/ChatSearch.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex w-full items-center justify-center p-4">
+        <input type="text" class="bg-gray-200 h-full w-full text-gray-400 pl-3 text-sm" placeholder="Search"/>
+    </div>
+</template>

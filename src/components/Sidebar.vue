9785d4fee245038c361af97c3b2644b26fd3c013
TibebeJs <Tibebes.js@gmail.com>
2022-01-18 17:01:33 +0300

    feat: create sidebar (chat folders)

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="bg-slate-700 w-20 h-full flex flex-col">
+        <div class="flex w-20 h-16 items-center justify-center">
+            <div class="h-7 w-7 text-white">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                    />
+                </svg>
+            </div>
+        </div>
+
+        <div
+            class="flex w-20 h-16 items-center justify-center flex-col text-white"
+            v-for="folder in folders"
+            :key="`folder-${folder.id}`"
+        >
+            <div class="h-7 w-7 " v-html="folder.icon"></div>
+            <div class="text-xs">
+                {{ folder.name }}
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { reactive } from "vue"
+
+export default {
+    setup() {
+        const folders = reactive([
+            {
+                id: "1",
+                icon: `<svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-7 w-7"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                >
+                    <path
+                        d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z"
+                    />
+                    <path
+                        d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z"
+                    />
+                </svg>`,
+                name: "All chats"
+            },
+            {
+                id: "2",
+                icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
+  <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd" />
+</svg>`,
+                name: "Unread"
+            },
+            {
+                id: "3",
+                icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+</svg>`,
+                name: "Personal"
+            },
+            {
+                id: "4",
+                icon: `<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+</svg>`,
+                name: "Edit"
+            },
+        ])
+
+        return {
+            folders,
+        }
+    },
+}
+</script>

d3e1f7433fb9b321c4676e3f427189420827c0c1
TibebeJs <Tibebes.js@gmail.com>
2022-01-18 19:30:25 +0300

    feat: create the emojis selector

---
+++ src/components/EmojiButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex items-center justify-center h-11 w-11 hover:bg-gray-200 text-lg cursor-pointer">{{ emoji }}</div>
+</template>
+<script>
+export default {
+    props: ["emoji"],
+    setup() {
+
+    },
+}
+</script>
